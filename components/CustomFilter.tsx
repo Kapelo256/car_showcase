2abedb321b7efcf17d2c9a4bb45ee33aa4031c56
Kapelo256 <105801565+Kapelo256@users.noreply.github.com>
2025-09-03 21:24:06 +0100

    Final App

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -13,20 +13,27 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 import { CustomButtonProps, CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
 
-  const handleUpdateParams = () => {
-    const newPathname = "";
+  const handleUpdateParams = (e: {type: string, value: string}) => {
+    const newPathname = updateSearchParams(title, e.value.toLowerCase());
 
-    router.push(newPathname)
-  }
+    router.push(newPathname);
+  };
 
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox 
+        value={selected} 
+        onChange={(e) => {
+        setSelected(e);
+        handleUpdateParams(e);
+      }}
+        >
         <div className="relative w-fit z-10">
           <ListboxButton className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
